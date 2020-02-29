8ce2810a84436a2ae152fa6cc083486a6ce28a07
Ожиганова Алёна <ozhiganovaalena@mail.ru>
2020-02-29 13:22:56 +0300

    Add stack (Ozhiganova Alena 2013)

---
+++ SAOD/Stack/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*MyStack mystack = new MyStack(5);
+
+            System.Console.WriteLine(mystack.Size());
+            System.Console.WriteLine(mystack.Top());
+
+            mystack.Push(3);
+            mystack.Push(19);
+
+            System.Console.WriteLine(mystack.IsEmpty());
+            System.Console.WriteLine(mystack.Size());
+
+            mystack.Push(41);
+            mystack.Push(5);
+
+            while (!mystack.IsEmpty())
+            {
+                System.Console.WriteLine(mystack.Top());
+                mystack.Pop();
+            }
+
+            mystack.Pop();
+            System.Console.WriteLine(mystack.IsEmpty());*/
+
+            var s = new Stack<int>();
+            Console.WriteLine(s.IsEmpty());
+            //Console.WriteLine(s.Pop());
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            Console.WriteLine(s.IsEmpty());
+            Console.WriteLine(s.Pop());
+            s.Clear();
+            Console.WriteLine(s.IsEmpty());
+            Console.WriteLine(s.Peek());
+            Console.WriteLine(s.Pop());
+
+        }
+    }
+}
---
+++ SAOD/Stack/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack<T>
+    {
+        T[] data;
+
+        public int Count { get; private set; }
+
+        public Stack()
+        {
+            data = new T[0];
+        }
+
+        public void Push(T s)
+        {
+            Array.Resize(ref data, Count + 1);
+            data[Count++] = s;
+        }
+
+        public T Peek()
+        {
+            if (Count > 0)
+                return data[Count - 1];
+            else
+                return default(T);
+        }
+
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+        public T Pop()
+        {
+            if (Count > 0)
+            {
+                var t = Peek();
+                Array.Resize(ref data, Count - 1);
+                --Count;
+                return t;
+            }
+            else
+                throw new System.InvalidCastException("Стек пуст");
+        }
+        public void Clear()
+        {
+            Count = 0;
+            Array.Resize(ref data, Count);
+        }
+    }
+}
